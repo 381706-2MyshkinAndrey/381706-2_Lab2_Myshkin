b533af11f1b469f9dc261fa98a04930048973122
381706-2MyshkinAndrey <43260478MyshkinAndrey@users.noreply.github.com>
2019-05-22 20:56:41 +0300

    Some good things

--- vector/main.cpp
+++ vector/main.cpp
@@ -5,18 +5,21 @@
 int main(int argc, char* argv[]) 
 {
   setlocale(LC_ALL, "");
-  TMatrix<int> a(5), b(5), c(5);
+  TMatrix<int> a(5), b(5), c(5), d(5);
   int i, j;
   cout << "Тестирование программ поддержки треугольных матриц" << endl;
   for (i = 0; i < 5; i++)
     for (j = i; j < 5; j++) 
     {
-      a[i][j] = i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
+      a[i][j] = rand() % 20 ;
+      b[i][j] = rand() % 30 ;
     }
   c = a + b;
+  d = a * b;
+
   cout << "Matrix a = " << endl << a << endl;
   cout << "Matrix b = " << endl << b << endl;
   cout << "Matrix c = a + b" << endl << c << endl;
+  cout << "Matrix c = a * b" << endl << d << endl;
   return 0;
 }
